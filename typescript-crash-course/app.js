3c03a0c322ce8aebf235ed5c6cace10bcbe227e5
Debasis Saikia <debasishsaikia136@gmail.com>
2021-05-26 15:30:25 +0530

    tuples

--- typescript-crash-course/app.js
+++ typescript-crash-course/app.js
@@ -10,6 +10,14 @@
 var person = {
     name: "Debasis Saikia",
     age: 24,
-    role: "UI lead and frontend engineer"
+    roles: ["UI lead", "Frontend Engineer"]
 };
-console.log(person.role);
+// type inferencing
+var roles; //array of string
+roles = ["UI dev"]; //only string can be added, any other data types will show an error ,'any' can be used instead of string to add multiple data types
+console.log(person.roles);
+// accessing array with for loop
+for (var _i = 0, _a = person.roles; _i < _a.length; _i++) {
+    var role = _a[_i];
+    console.log(role.toUpperCase());
+}
--- typescript-crash-course/app.ts
+++ typescript-crash-course/app.ts
@@ -9,10 +9,28 @@
 // console.log(person);
 
 // ===================better syntax
-const person = {
+const person: {
+  name: string;
+  age: number;
+  roles: string[];
+  tup: [number, string];
+} = {
   name: "Debasis Saikia",
   age: 24,
-  role: "UI lead and frontend engineer",
+  roles: ["UI lead", "Frontend Engineer"], //array of string
+  tup: [1, "Learn"], //tuples are of fixed length and types,we cannot assign other type fo value or add new elements
 };
 
-console.log(person.role);
+person.tup.push("More"); //we can do this
+// person.tup[1]=10 we cannot do this or person.tup=[0,'learn','more']
+
+// type inferencing
+let roles: string[]; //array of string
+roles = ["UI dev"]; //only string can be added, any other data types will show an error ,'any'[] can be used instead of string[] to add multiple data types
+
+console.log(person.roles);
+
+// accessing array with for loop
+for (const role of person.roles) {
+  console.log(role.toUpperCase());
+}
