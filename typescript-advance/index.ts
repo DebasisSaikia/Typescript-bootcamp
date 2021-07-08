0c3e326c438a62bca0c6a68736c86e94e93aa7b7
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-08 23:05:53 +0530

    unions and generics

--- typescript-advance/index.ts
+++ typescript-advance/index.ts
@@ -25,11 +25,30 @@ console.log(mixed)
 // defining types with interface
 
 interface User {
-    name: String;
-    id: Number;
+    name: string;
+    id: number;
 }
 
 const user: User = {
-    name: 'Debasis',
+    name: 'debasis',
     id: 1
 }
+
+// Composing Types
+// With TypeScript, you can create complex types by combining simple ones.There are two popular ways to do so: with Unions, and with Generics.
+// 1) Unions
+function getLength(obj: string | string[]) {
+    return obj.length;
+}
+
+// 2)Generics
+type strArr = Array<string>
+
+
+// structural type system -In a structural type system, if two objects have the same shape, they are considered to be of the same type.
+interface Dimension {
+    x: number;
+    y: number
+}
+
+const dimension = { x: 2, y: '3', z: 'dsjd' }
