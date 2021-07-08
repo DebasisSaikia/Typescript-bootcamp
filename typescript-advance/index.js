c4ae7cf55386c7114cc026c23fb60fae4a66b059
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-08 22:04:06 +0530

    generics and interfaces

--- typescript-advance/index.js
+++ typescript-advance/index.js
@@ -1,5 +1,18 @@
+// types basic
 var circ = function (diameter) {
     console.log(typeof (diameter));
     return diameter * Math.PI;
 };
 console.log(circ(20));
+// arrays and objects
+var names = ['debasis', 'tilo', 'biki'];
+names.push('saikia');
+console.log(names);
+// explicit types
+var age;
+var isTall;
+var str;
+// unions type
+var mixed = [];
+mixed.push('debasis', 20, true);
+console.log(mixed);
--- typescript-advance/index.ts
+++ typescript-advance/index.ts
@@ -1,6 +1,35 @@
 
+// types basic
 const circ = (diameter: number) => {
     console.log(typeof (diameter))
     return diameter * Math.PI
 }
 console.log(circ(20))
+
+// arrays and objects
+let names = ['debasis', 'tilo', 'biki'];
+names.push('saikia')
+console.log(names)
+
+// explicit types
+let age: number;
+let isTall: boolean;
+let str: string;
+
+// unions type
+let mixed: (string | number | boolean)[] = []
+
+mixed.push('debasis', 20, true)
+console.log(mixed)
+
+// defining types with interface
+
+interface User {
+    name: String;
+    id: Number;
+}
+
+const user: User = {
+    name: 'Debasis',
+    id: 1
+}
