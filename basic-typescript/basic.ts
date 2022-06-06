d0e3cc48ef7c1e648b4c0f9e3651dc13efcb409e
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-06 13:45:05 +0530

    basics types

--- basic-typescript/basic.ts
+++ basic-typescript/basic.ts
@@ -1,3 +1,37 @@
 const nam:string="Debasis"
 
 console.log(nam)
+
+let MyReg:RegExp=/dev/
+
+
+const names:string[]=nam.split(" ")
+const rev=names.reverse()
+
+// object
+
+interface val{
+    firstName:string,
+    age:number,
+    cool?:boolean
+}
+
+const obj:val={
+    firstName:'Debasis',
+    age:25,
+}
+//or
+// const obj:{firstName:string,age:number}={
+//     firstName:'Debasis',
+//     age:25,
+// }
+
+// objects mapping
+const ids:Record<number,string>={
+    10:"a",
+    20:"b",
+}
+
+ids[30]="c"
+
+console.log(obj.firstName.slice(0,2))
