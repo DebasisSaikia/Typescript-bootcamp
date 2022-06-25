4062b2bd283da57eef7f7cdb6e9f5d24e3c0c8f3
DebasisSaikia <debasishsaikia136@gmail.com>
2022-06-25 21:57:50 +0530

    project setup

--- micro-frontend/container/webpack.config.js
+++ micro-frontend/container/webpack.config.js
@@ -2,19 +2,18 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 const ModuleFederationPlugin=require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports={
-    mode:"development",
+    mode:'development',
     devServer:{
-        port:3001
+        port:3003
     },
     plugins:[
         new HtmlWebpackPlugin({
             template:'./public/index.html'
         }),
         new ModuleFederationPlugin({
-            name:'product',
-            filename:'remoteEntry.js',
-            exposes:{
-                './ProductIndex':'./src/index'
+            name:'container',
+            remotes:{
+                product:'product@http://localhost:3001/remoteEntry.js'
             }
         })
     ]
